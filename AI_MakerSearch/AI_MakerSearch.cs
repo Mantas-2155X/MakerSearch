a44c405707f14658f9fd848eff2f601d8a813793
Mantas-2155X <syenergyy@gmail.com>
2020-07-06 01:42:22 +0300

    * AI port * Removed ID search, replaced AssetName with AssetBundle search

---
+++ AI_MakerSearch/AI_MakerSearch.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+
+using BepInEx;
+using BepInEx.Configuration;
+
+using HarmonyLib;
+
+using CharaCustom;
+using SuperScrollView;
+using UnityEngine;
+
+namespace AI_MakerSearch
+{
+    [BepInPlugin(nameof(AI_MakerSearch), nameof(AI_MakerSearch), VERSION)]
+    public class AI_MakerSearch : BaseUnityPlugin
+    {
+        public const string VERSION = "1.0.0";
+
+        public static string searchString;
+        
+        public static CvsH_Hair cvsHair;
+        public static CvsC_Clothes cvsClothes;
+        public static CvsA_Slot cvsAccessories;
+        
+        public static LoopListView2 view;
+        public static CustomSelectScrollController controller;
+        
+        public static Tools.SearchCategory category;
+        
+        public static ConfigEntry<bool> caseSensitive { get; private set; }
+        public static ConfigEntry<bool> useTranslatedCache { get; private set; }
+        
+        public static ConfigEntry<Tools.SearchBy> searchBy { get; private set; }
+        
+        private void Awake()
+        {
+            caseSensitive = Config.Bind(new ConfigDefinition("General", "Case sensitive"), false);
+            useTranslatedCache = Config.Bind(new ConfigDefinition("General", "Search translated cache"), true, new ConfigDescription("Search in translated cache, if nonexistant then translate. Only works when search includes name"));
+            searchBy = Config.Bind(new ConfigDefinition("General", "Search by"), Tools.SearchBy.Name);
+
+            category = Tools.SearchCategory.None;
+            
+            var harmony = new Harmony(nameof(AI_MakerSearch));
+            harmony.PatchAll(typeof(Hooks));
+        }
+
+        public static void Search()
+        {
+            if (!Tools.UpdateUI(category))
+                return;
+
+            if (searchString == "")
+                return;
+
+            var trav = Traverse.Create(controller);
+            var datas = trav.Field("scrollerDatas").GetValue<CustomSelectScrollController.ScrollData[]>();
+
+            var datalist = datas.ToList();
+            foreach (var data in datalist.ToArray())
+            {
+                if(Tools.ItemMatchesSearch(data.info, searchString))
+                    continue;
+
+                if (controller.selectInfo == data)
+                    controller.SelectInfoClear();
+
+                datalist.Remove(data);
+            }
+            datas = datalist.ToArray();
+
+            trav.Field("scrollerDatas").SetValue(datas);
+
+            view.ReSetListItemCount(Mathf.CeilToInt((float)datas.Length / trav.Field("countPerRow").GetValue<int>()));
+        }
+    }
+}
---
+++ AI_MakerSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AI_MakerSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("AI_MakerSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("812FF1EB-384F-433A-89B5-E406CD32EFDD")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(AI_MakerSearch.AI_MakerSearch.VERSION)]
+[assembly: AssemblyFileVersion(AI_MakerSearch.AI_MakerSearch.VERSION)]
