ed0e99326f1bbe813302ecd7e8585abf821ad744
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2021-05-28 10:10:22 +0300

    * AssemblyInfo

---
+++ KKS_MakerSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KKS_MakerSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("KKS_MakerSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59B2D826-C44F-4D22-8381-5BAEFD0FC986")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KKS_MakerSearch.KKS_MakerSearch.VERSION)]
+[assembly: AssemblyFileVersion(KKS_MakerSearch.KKS_MakerSearch.VERSION)]
