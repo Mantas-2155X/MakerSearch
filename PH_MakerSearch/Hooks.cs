52f4585e54165fd62f86ce462ee5bef10ed7868a
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2021-08-29 11:37:15 +0300

    [PH] Recode some parts

--- PH_MakerSearch/Hooks.cs
+++ PH_MakerSearch/Hooks.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Timers;
 using System.Collections.Generic;
-
-using BepInEx;
 using HarmonyLib;
-
-using UniRx;
-using UniRx.Triggers;
 
 namespace PH_MakerSearch
 {
@@ -16,34 +9,9 @@ namespace PH_MakerSearch
         private static void EditMode_Setup_CreateUI(EditMode __instance, MoveableThumbnailSelectUI ___itemSelectUI)
         {
             Tools.CreateUI(__instance, ___itemSelectUI);
-            SetupCache(__instance);
+            Cacher.SetupCache(__instance);
         }
         
-        private static Timer _cacheSaveTimer;
-
-        private static void SetupCache(EditMode __instance)
-        {
-            Cacher.ReadCache();
-
-            void OnSave(object sender, ElapsedEventArgs args)
-            {
-                _cacheSaveTimer.Stop();
-                Cacher.WriteCache();
-            }
-
-            // Timeout has to be long enough to ensure people with potato internet can still get the translations in time
-            _cacheSaveTimer = new Timer(TimeSpan.FromSeconds(60).TotalMilliseconds);
-            _cacheSaveTimer.Elapsed += OnSave;
-            _cacheSaveTimer.AutoReset = false;
-            _cacheSaveTimer.SynchronizingObject = ThreadingHelper.SynchronizingObject;
-
-            // If a cache save is still pending on maker exit, run it immediately
-            __instance.OnDestroyAsObservable().Subscribe(_ =>
-            {
-                if (_cacheSaveTimer.Enabled) OnSave(null, null);
-            });
-        }
-
         [HarmonyPostfix, HarmonyPatch(typeof(ThumbnailSelectUI), "SetupCells")]
         private static void CustomSelectListCtrl_SetupCells_GetSelectInfos(List<CustomSelectSet> ___datas)
         {
@@ -76,8 +44,8 @@ namespace PH_MakerSearch
             if (anyTranslations)
             {
                 // Reset the timer so it's counting since the last translation
-                _cacheSaveTimer.Stop();
-                _cacheSaveTimer.Start();
+                Cacher._cacheSaveTimer.Stop();
+                Cacher._cacheSaveTimer.Start();
             }
         }
         
